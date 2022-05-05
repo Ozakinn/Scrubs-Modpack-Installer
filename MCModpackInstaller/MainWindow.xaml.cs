ef50ce1d2fe9b05d2a70a419171b176eff897c30
Krystler Cataniag <krystlerc@yahoo.com>
2022-05-05 17:14:45 +0800

    Adding Advanced settings

--- MCModpackInstaller/MainWindow.xaml.cs
+++ MCModpackInstaller/MainWindow.xaml.cs
@@ -20,9 +20,12 @@ namespace MCModpackInstaller
     /// </summary>
     public partial class MainWindow : Window
     {
+        int ozakiClickCount = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
@@ -34,6 +37,16 @@ namespace MCModpackInstaller
         private void btnExit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void btnOzaki_Click(object sender, RoutedEventArgs e)
+        {
+            ozakiClickCount++;
+            if (ozakiClickCount == 10)
+            {
+                AdvanceSettings advWPF = new AdvanceSettings();
+                advWPF.Show();
+            }
         }
     }
 }
